6f1f95de4200fb8784379054037d77c6ca93bf26
Anna <bogatyreva_aa@mail.ru>
2022-04-14 17:14:30 +0300

    v1.3.4 fix close

--- vendor/close_account.php
+++ vendor/close_account.php
@@ -13,12 +13,42 @@
 		header("Location: ../operwork.php#close_account");
 		return;
 	}
-			
 	$mysqli = get_sql_connection();
+	$stmt = $mysqli->prepare("SELECT currency, `default`, idclient FROM account WHERE accountnum = ?");
+	$stmt->bind_param("s", $_POST["accountnum"]);
+	$stmt->execute();
+	$data = $stmt->get_result()->fetch_row();
+	$currency = $data[0];
+	$default = $data[1];
+	$isclient = $data[2];
+	
+	$stmt = $mysqli->prepare("SELECT count(*) FROM account WHERE idclient = ? AND closed = '0000-00-00' AND currency = ?");
+	$stmt->bind_param("is", $idclient, $currency);
+	$stmt->execute();
+	$cntaccount = $stmt->get_result()->fetch_row()[0];
+
+	$mysqli->query("BEGIN");
+
+	if ($default == 1 && $cntaccount > 1) {
+		$stmt = $mysqli->prepare("SELECT accountnum FROM account WHERE idclient = ? AND `default` = 0 AND currency = ? LIMIT 1");
+		$stmt->bind_param("is", $idlient, $currency);
+		$stmt->execute();
+		$new_default = $stmt->get_result()->fetch_row()[0];
+	
+		$stmt = $mysqli->prepare("UPDATE account SET `default` = 1 WHERE accountnum = ?");
+		$stmt->bind_param("s", $new_default);
+		$stmt->execute();	
+	}
+	$stmt = $mysqli->prepare("UPDATE account SET `default` = 0 WHERE accountnum = ?");
+	$stmt->bind_param("s", $_POST["accountnum"]);
+	$stmt->execute();
+
 	$stmt = $mysqli->prepare("UPDATE account SET closed = (SELECT operdate FROM operdays WHERE current = 1) WHERE accountnum = ?");
 	$stmt->bind_param("s", $_POST["accountnum"]);
 	$stmt->execute();
 	
+	$mysqli->query("COMMIT");
+
 	$_SESSION["message-close"] = "Счет закрыт.";
 	header("Location: ../operwork.php#close_account");
 			
