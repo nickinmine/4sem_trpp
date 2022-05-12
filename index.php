ae81ede6f64e28f6993d0a2f9d80350884c42dbb
Nick <nick.a.zubov@gmail.com>
2022-05-12 15:10:14 +0300

    redirect fix

--- index.php
+++ index.php
@@ -2,13 +2,11 @@
 	require "vendor/lib.php";
 
 	safe_session_start();
-	if (in_array("user", $_SESSION) && in_array("role", $_SESSION["user"])) {
-		if (($_SESSION["user"]["role"] == "admin") || ($_SESSION["user"]["role"] == "operator")) {
-			header('Location: /oper.php');
-		}
-		else if ($_SESSION["user"]["role"] == "accountant") {
-			header('Location: /acc.php');
-		}
+	if (($_SESSION["user"]["role"] == "admin") || ($_SESSION["user"]["role"] == "operator")) {
+		header('Location: /oper.php');
+	}
+	else if ($_SESSION["user"]["role"] == "accountant") {
+		header('Location: /acc.php');
 	}
 ?>
 <!DOCTYPE html>
@@ -25,7 +23,7 @@
 <div class="app-router-container">
 	<div class="auth-form">
 		<form action="vendor/signin.php" method="post">
-			<p class="form-name">ТРПП Банк<br>Версия release-1.5.2</p>
+			<p class="form-name">ТРПП Банк</p>
 			<label>Логин</label>
 			<label>
 				<input type="text" name="login" placeholder="Введите логин" required>
